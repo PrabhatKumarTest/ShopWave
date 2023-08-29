d3b11f32aa03490d21252b366a5ea3c6c1890cf3
PrabhatKumarTest <143084541+PrabhatKumarTest@users.noreply.github.com>
2023-08-29 23:32:18 +0000

    Login/Signup page setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 // import HomePage from './component/pages/HomePage';
 // import ProductPage from './component/pages/ProductPage';
-import Cart from './component/pages/Cart';
+// import Cart from './component/pages/Cart';
+import Login from './component/pages/Login';
+import Signup from './component/pages/Signup';
 
 
 function App() {
   return (
     //  <HomePage/>
     //  <ProductPage/>
-    <Cart />
+    // <Cart />
+    // <Login/>
+    <Signup/>
   );
 }
 
