970945e5a62ed0ba36084e84acf62664a753beff
PrabhatKumarTest <143084541+PrabhatKumarTest@users.noreply.github.com>
2023-08-30 20:57:06 +0000

    Crousel/ success/ Failed page setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-// import HomePage from './component/pages/HomePage';
+import HomePage from './component/pages/HomePage';
 // import ProductPage from './component/pages/ProductPage';
 // import Cart from './component/pages/Cart';
-import Login from './component/pages/Login';
-import Signup from './component/pages/Signup';
+// import Login from './component/pages/Login';
+// import Signup from './component/pages/Signup';
+// import Checkout from './component/pages/Checkout';
 
 
 function App() {
   return (
-    //  <HomePage/>
+     <HomePage/>
     //  <ProductPage/>
     // <Cart />
     // <Login/>
-    <Signup/>
+    // <Signup/>
+    // <Checkout />
   );
 }
 
--- src/component/pages/HomePage.js
+++ src/component/pages/HomePage.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './../Navbar';
 import ItemsContainer from './../ItemsContainer';
 import Footer from './../Footer';
+import Carousel from '../Carousel.js';
 
 const HomePage = () => {
     return (
         <div>
             <>
                 <Navbar />
+                <Carousel/>
                 <ItemsContainer />
                 <Footer/>
             </>
